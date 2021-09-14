0d1e3a92f60a26a0c324a765e5273bad7002ca66
csr5321 <cradford1@mail.sfsu.edu>
2021-09-13 19:24:20 -0700

    paths checked

--- application/server.js
+++ application/server.js
@@ -17,7 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
 app.use(express.static('./public'));
 
 // -------------------------------------------------
