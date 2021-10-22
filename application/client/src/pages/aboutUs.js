2591ad2c88632c0d600eac14a90c403ae1dac807
James Giatpaiboon <jgiatpaiboon@mail.sfsu.edu>
2021-10-22 13:03:16 -0700

    sw prototype

---
+++ application/client/src/pages/aboutUs.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import { NavLink } from "react-router-dom";
+
+const aboutUs = () =>{
+    return(
+<div> 
+    <h1>About Us - Team 5</h1>
+    <div>
+      <p> some text here</p>
+    </div>
+
+</div>
+    );
+}
+export default aboutUs;
---
+++ application/client/src/pages/createAccount.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import './createAccount.css';
+
+import axios from 'axios';
+
+
+function CreateAccount () {
+
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [userID, setUserID] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = async (e) => {
+        await axios.post(`/newAccount`, {
+          fullName,
+          email,
+          userID,
+          password,
+        });
+    }
+    
+    return (
+
+    <form onSubmit={submitHandler}>
+        <h1>Create Account</h1>
+
+        <label>
+            Full Name:
+            <input
+            name="Full Name"
+            placeholder='Full Name'
+            type='text'
+            value={fullName}
+            onChange={e => setFullName(e.target.value)}
+             />
+        </label>
+        <label>
+            Email Address:
+        <input
+        name="Email Address"
+        placeholder='Email Address'
+        type='text'
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        />
+        </label>
+        <label>
+            User ID:
+            <input
+            name="User ID"
+            placeholder='User ID'
+            type='text'
+            value={userID}
+            onChange={e => setUserID(e.target.value)}
+            />
+        </label>
+        <label>
+            Password:
+            <input
+            name="Password"
+            placeholder='Password'
+            type='password'
+            value={password}
+            onChange={e => setUserID(e.target.value)}
+            />
+        </label>
+        <button>Create Account</button>
+    
+        </form>
+    
+    );
+}
+
+export default CreateAccount;
