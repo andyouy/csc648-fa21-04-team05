be9167ec3447b1d6a6c52229a6a6404545f01993
JaguarSaggar <66286609+jaguarsaggar@users.noreply.github.com>
2021-11-07 10:06:30 -0800

    App.js after added footer

---
+++ application/client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './Navbar';
+import "./pages/aboutUs"
+import './App.css';
+
+import Home from './pages/home';
+import aboutUs from './pages/aboutUs';
+import createAccount from './pages/createAccount';
+import login from './pages/login';
+import Footer from './pages/Footer';
+
+function App() {
+  return (
+
+    <div className="App">
+    <h1> Shyft </h1>
+ 
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/aboutUs' exact component={aboutUs}/>
+        <Route path='/createAccount' exact component={createAccount}/>
+        <Route path='/login' component={login}/>
+      </Switch>
+
+    </Router>
+      <Footer />
+    </div>
+  );
+}
+export default App;
