9d39ebb5e5d6fe874ea2f1c4f6c66d0c392b78d8
csr5321 <cradford1@mail.sfsu.edu>
2021-11-08 10:52:51 -0800

    removed footer.css, added assets directory for css, moved navbar and footer to components directory

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { useEffect, useState} from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import "./pages/aboutUs"
-import './App.css';
+import './assets/App.css';
 
 import Home from './pages/home';
 import aboutUs from './pages/aboutUs';
-
-
 import MainCreateAccount from './pages/mainCreateAccount';
 import Login from './pages/login';
-
 
 import James from './pages/AboutUs/James';
 import Mohammad from './pages/AboutUs/Mohammad';
@@ -48,27 +46,22 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/aboutUs' component={aboutUs}/>
-
-        <Route path='/login' component={Login}/>
-        
-        <Route path='/mainCreateAccount' component={MainCreateAccount}></Route>
-
-        <Route path='/employeeAccount' component={CreateEmployeeAccount}></Route>
-        <Route path='/employerAccount' component={CreateEmployerAccount}></Route>
-
-
-        <Route path='/employerDashboard' component={EmployerDashboard}></Route>
-
-
         <Route path='/James' component={James}></Route>
         <Route path='/Mohammad' component={Mohammad}></Route>
         <Route path='/Ana' component={Ana}></Route>
         <Route path='/Andy' component={Andy}></Route>
         <Route path='/Courtney' component={Courtney}></Route>
+        <Route path='/login' component={Login}/>
+        
+        <Route path='/mainCreateAccount' component={MainCreateAccount}></Route>
+        <Route path='/employeeAccount' component={CreateEmployeeAccount}></Route>
+        <Route path='/employerAccount' component={CreateEmployerAccount}></Route>
+        <Route path='/employerDashboard' component={EmployerDashboard}></Route>
 
       </Switch>
 
     </Router>
+    <Footer />
     </div>
   );
 }
--- application/client/src/components/Footer.js
+++ application/client/src/components/Footer.js
@@ -1,5 +1,4 @@
 ﻿import React from 'react';
-import "./Footer.css"
 
 const Footer = () => {
     return (
