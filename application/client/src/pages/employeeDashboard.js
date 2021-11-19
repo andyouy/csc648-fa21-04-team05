4786521fd95c1ef64002713ae9f01062ef0f0ca3
csr5321 <cradford1@mail.sfsu.edu>
2021-11-19 12:52:14 -0800

    oops

--- application/client/src/pages/employeeDashboard.js
+++ application/client/src/pages/employeeDashboard.js
@@ -1,52 +1,19 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import { useHistory, Link } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom";
 
 
-function EmployeeDashboard() {
+function EmployeeDashboard(){
 
-    const [users, setUsers] = useState([]);
-
-    let history = useHistory;
-
-    // const dashboardRouter = (id) => {
-    //     axios.get("/api/login", {withCredentials: true}).then((response) =>{
-    //       updateUserState(response.data.isBusinessAccount);
-    //       updateLoginState(null);
-    //       history.go(0);
-    //     })
-    //     .catch((err) =>{
-    //       console.log(err);
-    //     });
-    
-    //   }
-    
-    const isBusinessAccount = () => {
-        axios.get(users.map)
-    }
-
-    
 return(
     <div className="content-wrap">
+        <h1>Dashboard</h1>
 
-        <h1>Employee Dashboard</h1><hr/>
-
-        {/* {!isBusinessAccount ? */}
-            <Link to="/employeeFind"><button className="btn-choice"><span><h3>Claim Shift</h3></span></button></Link>
-            {/* : */}
-            {/* <Link to="/createShyft"><button className="btn-choice"><span><h3>Create Shift</h3></span></button></Link> */}
-        {/* } */}
-
-        {/* {!isBusinessAccount ? */}
-            <Link to="/employeeViews"><button className="btn-choice"><span><h3>View Claimed</h3></span></button></Link>
-            {/* : */}
-            {/* <Link to="/employerViews"><button className="btn-choice"><span><h3>View Unclaimed</h3></span></button></Link> */}
-        {/* } */}
-
-            {/* {!isBusinessAccount ? <Link to="/login">Login</Link> : <Link to="/" onClick={dashboardRouter}> Logout</Link>}
-            {!isBusinessAccount ? <Link to="/login">Login</Link> : <Link to="/" onClick={dashboardRouter}> Logout</Link>} */}
-
+            <Link to="/employeeFind">
+                <button ><span><h3>Pick Up</h3></span></button>
+            </Link>
+            <Link to="/employeeViews">
+                <button ><span><h3>View Scheduled</h3></span></button>
+            </Link>
     </div>
 )
 
