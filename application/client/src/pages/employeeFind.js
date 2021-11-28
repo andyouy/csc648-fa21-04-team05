5a5b19677f07ae7e45c65d6725f0fccbd4519047
Mohammad Khan, mk1159 <mkhan7@mail.sfsu.edu>
2021-11-28 15:42:30 -0800

    edited shift tile UI, added classes to app.css for buttons

--- application/client/src/pages/employeeFind.js
+++ application/client/src/pages/employeeFind.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {  useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function EmployeeFind() {
     const [shifts, setShifts] = useState([]);
@@ -8,43 +8,42 @@ function EmployeeFind() {
     let history = useHistory();
 
     useEffect(() => {
-    axios.get("/api/getAllShifts")
-    .then((response) => {
-        setShifts(response.data);
-        console.log(shifts)
-        })
+        axios.get("/api/getAllShifts")
+            .then((response) => {
+                setShifts(response.data);
+                console.log(shifts)
+            })
     }, []);
 
     const claimShift = (id) => {
-        axios.put("/api/claimShift", {id: id})
-        .then((response) => {
-            if(response) {
-                history.go(0)
-            }
-        })
+        axios.put("/api/claimShift", { id: id })
+            .then((response) => {
+                if (response) {
+                    history.go(0)
+                }
+            })
     }
 
-      return(
+    return (
         <div className="content-wrap">
             <div>
                 <h1>BROWSE AVAILABLE SHIFTS</h1>
-                <hr/>
+                <hr />
             </div>
             <div className="available-shifts">
-                    {shifts.map((val,key) =>
-                {
+                {shifts.map((val, key) => {
                     return (
                         <div className="shift">
                             <div>
-                                <h3>date: {val.date}</h3>
-                                <h2>${val.minPay}.00</h2>
-                                <h3>seeking: {val.title} @ {val.createdBy}</h3>
-                                <h3>start time: {val.time}</h3>
-                                <h3>address: {val.location}</h3>
+                                <h2 class="shift-tile-pay">${val.minPay}.00</h2>
+                                <h3 class="shift-tile-date">Date: {val.date}</h3>
+                                <h3>Seeking: {val.title} @ {val.createdBy}</h3>
+                                <h3>Start Time: {val.time}</h3>
+                                <h3>Address: {val.location}</h3>
                             </div>
 
                             <div>
-                                <button className="btn-submit" onClick={()=> claimShift(val.shiftID)}>Claim</button>
+                                <button class="btn-claim-shift" onClick={() => claimShift(val.shiftID)}>Claim</button>
                             </div>
                         </div>
 
@@ -52,7 +51,7 @@ function EmployeeFind() {
                 })}
             </div>
         </div>
-      );
+    );
 }
 
 export default EmployeeFind;
